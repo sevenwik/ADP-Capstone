c0efe9e7b502c5d981ad9da6ca704067f91bd805
sevenwik <sathwikchepyala99@gmail.com>
2024-08-30 14:05:26 -0400

    Fixed the form handle

--- talent-client/src/pages/Login.jsx
+++ talent-client/src/pages/Login.jsx
@@ -1,66 +1,65 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export const Login = () => {
-    
-    const [formState, setFormState] = useState({email: "", password: ""})
+  const [formState, setFormState] = useState({ username: "", password: "" });
 
-    /* useEffect(() => {
-        setFormState({})
-    }, [{}]); */
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target
-        setFormState(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("in submit");
+    console.log(formState);
+  };
 
-    const handleSubmit = () => {
-        console.log("in submit")
-        console.log(formState)
-    }
-
-    return (
-        <>
-            <h1>Login</h1>
-            <form>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  name="name"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  name="pass"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="form-check form-switch">
-                <input 
-                    className="form-check-input" 
-                    type="checkbox" 
-                    role="switch" 
-                    id="flexSwitchCheckDefault"/>
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Show password</label>
-              </div>
-              <button type="submit" className="btn btn-primary" onSubmit={handleSubmit}>
-                Submit
-              </button>
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Username
+          </label>
+          <input
+            type="username"
+            className="form-control"
+            id="exampleInputEmail1"
+            name="username"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="password"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-check form-switch">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+          />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+            Show password
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
